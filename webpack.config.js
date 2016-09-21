0bf2b1c874ae0cabd2e0dd7eadb2e9e46022623c
jasmo2 <jasmo2.com@gmail.com>
2016-09-21 10:27:09 -0500

    update styles and web pack compilation

--- webpack.config.js
+++ webpack.config.js
@@ -10,49 +10,48 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var webpack = require('webpack');
 
 module.exports = {
-	  entry: {
-			     "app": [
-								SRC_DIR + "/css/semantic.css",
- 								JS_SRC_DIR + "/index.js"
-							],
-			       },
+	entry: {
+		"app": [
+			SRC_DIR + "/css/main.scss",
+			JS_SRC_DIR + "/index.js"
+		],
+	},
 
-	    resolve: {
-			         extensions: ["", ".js", ".jsx"]
-					   },
+	resolve: {
+		extensions: ["","scss", ".js", ".jsx"]
+	},
 
-	      module: {
-			          loaders: [
-					        {
-							        test: /\.jsx?$/,
-								        loader: "babel",
-									        include: path.resolve(JS_SRC_DIR),
-										        exclude: /(node_modules|bower_components)/,
-											        query: {
-													          presets: ['react', 'es2015']
-															          }
-								      },
-				        {
-						        test: /\.scss$/,
-							        loaders: ["style", "css", "autoprefixer", "sass"],
-								        include: path.resolve(SRC_DIR),
-									      },
-					      {
-						              test: /\.css$/,
-							              loader: ExtractTextPlugin.extract("style", "css")
-									            },
-					            {
-							            test: /\.(png|svg|woff|woff2|ttf|eot)$/,
-								            loader: "url-loader"
-										          }
-				      ]
-					        },
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: "babel",
+				include: path.resolve(JS_SRC_DIR),
+				exclude: /(node_modules|bower_components)/,
+				query: {
+					presets: ['react', 'es2015']
+				}
+			},
+			{
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract('style', 'css!sass?indentedSyntax&includePaths[]=' + __dirname +  '/node_modules'),
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract("style", "css")
+			},
+			{
+				test: /\.(png|svg|woff|woff2|ttf|eot)$/,
+				loader: "url-loader"
+			}
+		]
+	},
 
-	        plugins: [
-					    new ExtractTextPlugin("./app.css")
-		      ],
-		  output: {
-			      path: DEST_DIR ,
-			          filename: "app.js"
-					    }
+	plugins: [
+		new ExtractTextPlugin("./app.css")
+	],
+	output: {
+		path: DEST_DIR ,
+		filename: "app.js"
+	}
 };
