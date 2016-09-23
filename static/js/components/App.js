b398b86f82c283b336abd2cfeb60c0fb789ceed3
jasmo2 <jasmo2.com@gmail.com>
2016-09-23 18:52:51 -0500

    refactor

--- static/js/components/App.js
+++ static/js/components/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import Navbar from './navbar'
 import Sidebar from './sidebar'
+import Me from './me-section'
 
 class App extends React.Component{
-        // <Navbar/>
         // <Sidebar/>
   render() {
     return (
-      <div className="main container">
-        <div className="ui segment">
-          <div className="ui segment">
-            <div className="ui right internal dividing rail">
-                Right Rail Content
-            </div>
-            <p></p>
-            <p></p>
-          </div>
-        </div>
-        <div className="ui sixteen column grid container">
-          <div className="ui ten wide centered column">
-            <h2 className="ui centered header">Hello React, Webpack, Semantic!</h2>
-          </div>
+      <div>
+        <Navbar/>
+        <div className="container">
+          <Me/>
         </div>
       </div>
     )
