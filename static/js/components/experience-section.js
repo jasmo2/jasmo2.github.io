41c18e4c76a18fc1c76b8d4dffab7100e510abba
jasmo2 <jasmo2.com@gmail.com>
2016-10-02 09:38:32 -0500

    add experience section and SCSS

--- static/js/components/experience-section.js
+++ static/js/components/experience-section.js
@@ -3,8 +3,39 @@ import React, {Component} from 'react';
 class Experience extends Component{
   render(){
     return (
-    <section id="experience" className="case experience">
-      <h1>Experience Section</h1>
+      <section id="experience" className="case experience">
+
+        <div className="ui centered grid">
+          <div className="row headerRow"><h1>Experience Section</h1></div>
+
+          <div className="sixteen wide tablet eight wide computer column">
+            <div className="ui tiny images">
+              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/image.png"/>
+            </div>
+          </div>
+          <div className="sixteen wide tablet eight wide computer column">
+            <div className="ui tiny images">
+              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/image.png"/>
+            </div>
+        </div>
+        <div className="sixteen wide tablet eight wide computer column">
+          <div className="ui tiny images">
+            <img className="ui image" src="./static/images/image.png"/>
+            <img className="ui image" src="./static/images/image.png"/>
+            <img className="ui image" src="./static/images/image.png"/>
+            <img className="ui image" src="./static/images/image.png"/>
+          </div>
+        </div>
+
+
+
+      </div>
     </section>
   )}
 }
