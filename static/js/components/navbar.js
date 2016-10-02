db9b97d9c97675e9f84a30e301dce8c2026d883a
jasmo2 <jasmo2.com@gmail.com>
2016-10-02 18:41:55 -0500

    refactor navbar style

--- static/js/components/navbar.js
+++ static/js/components/navbar.js
@@ -9,25 +9,63 @@ jQuery.fn.visibility = require('semantic-ui-visibility');
 
 class Navbar extends Component {
   componentDidMount(){
-    // jQuery(this.refs.mainMenu).visibility({
-    //        type: 'fixed'
-    //  });
+    jQuery('.ui.vertical.menu').toggle();
+    jQuery('.right.menu.open').on("click",function(e){
+       e.preventDefault();
+       jQuery('.ui.vertical.menu').toggle();
+     });
   }
   render(){
     return(
-      <div className="ui borderless main menu" ref="mainMenu" id="portafolioNav">
-        <div className="ui text container">
-          <a href="#me" className="header item">
-            How am I?
-          </a>
-          <a href="#profile" className="item">Profile</a>
-          <a href="#experience" className="item">Tecnologies and Experience</a>
-          <a href="#portafolio" className="item">Portafolio</a>
-          <a href="#contact" className="item">Contact</a>
+      <div className="ui grid navbargrid cero mleft mrigth" id="main-navbar">
+        <div className="computer tablet only row">
+          <div className="ui navbar-color fixed menu navbar page grid">
+            <a href="" className="brand item">Jasmo2</a>
+            <a href="#me" className="header item">
+              How am I?
+            </a>
+            <a href="#profile" className="item">Profile</a>
+            <a href="#experience" className="item">Tecnologies and Experience</a>
+            <a href="#portafolio" className="item">Portafolio</a>
+            <a href="#contact" className="item">Contact</a>
+          </div>
+        </div>
+        <div className="mobile only row">
+            <div className="ui fixed navbar-color navbar menu">
+                <a href="" className="brand item">Jasmo2</a>
+                <div className="right menu open">
+                    <a href="" className="menu item">
+                        <i className="content icon"></i>
+                    </a>
+                </div>
+            </div>
+            <div className="ui vertical navbar menu">
+                <a href="#me" className="header item">
+                  How am I?
+                </a>
+                <a href="#profile" className="item">Profile</a>
+                <a href="#experience" className="item">Tecnologies and Experience</a>
+                <a href="#portafolio" className="item">Portafolio</a>
+                <a href="#contact" className="item">Contact</a>
+            </div>
         </div>
       </div>
+
+
+
     )
   }
 }
+      // <div className="ui borderless main menu" ref="mainMenu" id="portafolioNav">
+      //   <div className="ui text container">
+      //     <a href="#me" className="header item">
+      //       How am I?
+      //     </a>
+      //     <a href="#profile" className="item">Profile</a>
+      //     <a href="#experience" className="item">Tecnologies and Experience</a>
+      //     <a href="#portafolio" className="item">Portafolio</a>
+      //     <a href="#contact" className="item">Contact</a>
+      //   </div>
+      // </div>
 
 export default Navbar ;
