7870427c3c798233deb62e26fff401bf0db395b3
jasmo2 <jasmo2.com@gmail.com>
2016-10-02 12:51:33 -0500

    add languages and framework images

--- static/js/components/experience-section.js
+++ static/js/components/experience-section.js
@@ -5,36 +5,80 @@ class Experience extends Component{
     return (
       <section id="experience" className="case experience">
 
-        <div className="ui centered grid">
+        <div className="ui grid centered">
           <div className="row headerRow"><h1>Experience Section</h1></div>
 
-          <div className="sixteen wide tablet eight wide computer column">
+          <div className="sixteen wide tablet eight wide computer column centered technologies">
             <div className="ui tiny images">
-              <img className="ui image" src="./static/images/image.png"/>
-              <img className="ui image" src="./static/images/image.png"/>
-              <img className="ui image" src="./static/images/image.png"/>
-              <img className="ui image" src="./static/images/image.png"/>
+              <img className="ui image" src="./static/images/ReactJS.png"/>
+              <img className="ui image" src="./static/images/Angular.png"/>
+            </div>
+            <div className="ui tiny images">
+              <span className="ui tag labels">
+                <span className="ui label">
+                  ReactJS
+                </span>
+                <span className="ui label">
+                  AngularJS – 1.X
+                </span>
+              </span>
             </div>
           </div>
-          <div className="sixteen wide tablet eight wide computer column">
+          <div className="sixteen wide tablet eight wide computer column centered technologies">
             <div className="ui tiny images">
-              <img className="ui image" src="./static/images/image.png"/>
-              <img className="ui image" src="./static/images/image.png"/>
-              <img className="ui image" src="./static/images/image.png"/>
-              <img className="ui image" src="./static/images/image.png"/>
+              <span>
+                <img className="ui image" src="./static/images/Python.png"/>
+              </span>
+              <span>
+                <img className="ui image" src="./static/images/Elixir.png"/>
+              </span>
+            </div>
+            <div className="ui tiny images">
+              <span className="ui tag labels">
+                <span className="ui label">
+                  Python
+                </span>
+                <span className="ui label">
+                  Elixir
+                </span>
+              </span>
             </div>
         </div>
-        <div className="sixteen wide tablet eight wide computer column">
+        <div className="sixteen wide tablet eight wide computer column centered technologies">
           <div className="ui tiny images">
-            <img className="ui image" src="./static/images/image.png"/>
-            <img className="ui image" src="./static/images/image.png"/>
-            <img className="ui image" src="./static/images/image.png"/>
-            <img className="ui image" src="./static/images/image.png"/>
+            <img className="ui image" src="./static/images/jQuery.png"/>
+            <img className="ui image" src="./static/images/js-logo.png"/>
+          </div>
+          <div className="ui tiny images">
+            <span className="ui tag labels">
+              <span className="ui label">
+                jQuery<br/>
+                $
+              </span>
+              <span className="ui label">
+                Vanilla Javascript <br/>
+              (  <i className="heart icon"> ES6</i>)
+              </span>
+            </span>
+          </div>
+
+        </div>
+        <div className="sixteen wide tablet eight wide computer column centered technologies">
+          <div className="ui tiny images">
+            <img className="ui image" src="./static/images/NodeJS.png"/>
+            <img className="ui image" src="./static/images/Ruby.png"/>
+          </div>
+          <div className="ui tiny images">
+            <span className="ui tag labels">
+              <span className="ui label">
+                NodeJS
+              </span>
+              <span className="ui label">
+                Ruby
+              </span>
+            </span>
           </div>
         </div>
-
-
-
       </div>
     </section>
   )}
