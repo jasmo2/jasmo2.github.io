beb73435be09315a4280cc76fa2c1e4b59456659
jasmo2 <jasmo2.com@gmail.com>
2016-10-02 22:04:44 -0500

    add portafolio section

--- static/js/components/portafolio-section.js
+++ static/js/components/portafolio-section.js
@@ -3,23 +3,28 @@ import React, {Component} from 'react';
 class Portafolio extends Component{
   render(){
     return (
-      <section id="portafolio" className="case experiportafolioence">
+      <section id="portafolio" className="case portafolio">
 
         <div className="ui grid centered">
           <div className="row headerRow"><h1>Tecnologies and Experience</h1></div>
           <div className="four wide computer six wide tablet column centered">
-            <a href="http://duckduckgo.com" className="ui medium image">
-              <img src="static/images/image.png"/>
+            <a href="http://help-esac.rhcloud.com/es/entrenamiento" className="ui medium image">
+              <img src="static/images/project-prometeo.png"/>
             </a>
           </div>
           <div className="four wide computer six wide tablet column centered">
-            <a href="http://duckduckgo.com" className="ui medium image">
-              <img src="static/images/image.png"/>
+            <a href="http://seamos.co/" className="ui medium image">
+              <img src="static/images/project-seamos.png"/>
             </a>
           </div>
           <div className="four wide computer six wide tablet column centered">
-            <a href="http://duckduckgo.com" className="ui medium image">
-              <img src="static/images/image.png"/>
+            <a href="http://soulcookies.co/" className="ui medium image">
+              <img src="static/images/project-soulcookies.png"/>
+            </a>
+          </div>
+          <div className="four wide computer six wide tablet column centered">
+            <a href="http://coonatra.herokuapp.com/" className="ui medium image">
+              <img src="static/images/project-coonatra.png"/>
             </a>
           </div>
       </div>
