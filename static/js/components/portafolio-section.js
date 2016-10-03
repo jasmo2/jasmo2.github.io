24968cbf059e7cf219a8a2a905dcb975afc4b0cd
jasmo2 <jasmo2.com@gmail.com>
2016-10-02 19:01:15 -0500

    add portafolio section

---
+++ static/js/components/portafolio-section.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Portafolio extends Component{
+  render(){
+    return (
+      <section id="portafolio" className="case experiportafolioence">
+
+        <div className="ui grid centered">
+          <div className="row headerRow"><h1>Tecnologies and Experience</h1></div>
+          <div className="sixteen wide">
+            <a href="http://duckduckgo.com" class="ui medium image">
+              <img src="static/images/image.png"/>
+            </a>
+          </div>
+      </div>
+    </section>
+  )}
+}
+
+export default Portafolio;
