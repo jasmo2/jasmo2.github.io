c3b88e36c8414d94b3f0f86edd73df2837c11dea
jasmo2 <jasmo2.com@gmail.com>
2016-10-12 15:12:48 -0500

    github production

--- app.js
+++ app.js
@@ -19825,6 +19825,10 @@
 
 	var _portafolioSection2 = _interopRequireDefault(_portafolioSection);
 
+	var _contactSection = __webpack_require__(185);
+
+	var _contactSection2 = _interopRequireDefault(_contactSection);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -19856,7 +19860,8 @@
 	          { className: 'container' },
 	          _react2.default.createElement(_meSection2.default, null),
 	          _react2.default.createElement(_experienceSection2.default, null),
-	          _react2.default.createElement(_portafolioSection2.default, null)
+	          _react2.default.createElement(_portafolioSection2.default, null),
+	          _react2.default.createElement(_contactSection2.default, null)
 	        )
 	      );
 	    }
@@ -36754,6 +36759,146 @@
 	}(_react.Component);
 
 	exports.default = Portafolio;
+
+/***/ },
+/* 185 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(16);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Contact = function (_Component) {
+	  _inherits(Contact, _Component);
+
+	  function Contact() {
+	    _classCallCheck(this, Contact);
+
+	    return _possibleConstructorReturn(this, (Contact.__proto__ || Object.getPrototypeOf(Contact)).apply(this, arguments));
+	  }
+
+	  _createClass(Contact, [{
+	    key: "render",
+	    value: function render() {
+	      return _react2.default.createElement(
+	        "section",
+	        { id: "contact", className: "case contact" },
+	        _react2.default.createElement(
+	          "div",
+	          { className: "ui grid centered" },
+	          _react2.default.createElement(
+	            "div",
+	            { className: "sexteen column row" },
+	            _react2.default.createElement(
+	              "div",
+	              { className: "twelve wide column" },
+	              _react2.default.createElement("br", null),
+	              _react2.default.createElement(
+	                "h1",
+	                { className: "ui header center aligned" },
+	                _react2.default.createElement(
+	                  "span",
+	                  null,
+	                  _react2.default.createElement("i", { className: "volume control phone icon" }),
+	                  _react2.default.createElement(
+	                    "div",
+	                    { className: "content" },
+	                    "¿ Wanna contact me ?"
+	                  )
+	                )
+	              )
+	            )
+	          ),
+	          _react2.default.createElement(
+	            "div",
+	            { className: "twelve wide column" },
+	            _react2.default.createElement(
+	              "div",
+	              { className: "twelve wide column" },
+	              _react2.default.createElement(
+	                "div",
+	                { className: "ui list" },
+	                _react2.default.createElement(
+	                  "div",
+	                  { className: "item" },
+	                  _react2.default.createElement("i", { className: "users icon" }),
+	                  _react2.default.createElement(
+	                    "div",
+	                    { className: "content" },
+	                    "By: ",
+	                    _react2.default.createElement(
+	                      "strong",
+	                      null,
+	                      "jasmo2"
+	                    )
+	                  )
+	                ),
+	                _react2.default.createElement(
+	                  "div",
+	                  { className: "item" },
+	                  _react2.default.createElement("i", { className: "marker icon" }),
+	                  _react2.default.createElement(
+	                    "div",
+	                    { className: "content" },
+	                    "Bogotá, Colombia"
+	                  )
+	                ),
+	                _react2.default.createElement(
+	                  "div",
+	                  { className: "item" },
+	                  _react2.default.createElement("i", { className: "mail icon" }),
+	                  _react2.default.createElement(
+	                    "div",
+	                    { className: "content" },
+	                    _react2.default.createElement(
+	                      "a",
+	                      { href: "mailto:jasmo2.com@gmail.com" },
+	                      "jasmo2.com@gmail.com"
+	                    )
+	                  )
+	                ),
+	                _react2.default.createElement(
+	                  "div",
+	                  { className: "item" },
+	                  _react2.default.createElement("i", { className: "linkify icon" }),
+	                  _react2.default.createElement(
+	                    "div",
+	                    { className: "content" },
+	                    _react2.default.createElement(
+	                      "a",
+	                      { href: "https://co.linkedin.com/in/jaime-molina-21592960" },
+	                      "linkedin CV"
+	                    )
+	                  )
+	                )
+	              )
+	            )
+	          )
+	        )
+	      );
+	    }
+	  }]);
+
+	  return Contact;
+	}(_react.Component);
+
+	exports.default = Contact;
 
 /***/ }
 /******/ ]);
